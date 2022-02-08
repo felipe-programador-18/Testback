f400a67ec51b4fffcb62e7e9b7a13b33ee45d23c
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-08 16:20:24 -0300

    c8- adding code javascript

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const api = express()
 const port =  process.env.PORT || 3000
 
+const inscricao = require('./lib/inscricao')
+
 const path = require('path')
 
 // it works with express.js
@@ -15,14 +17,27 @@ api.use(express.static(path.join(__dirname, 'public')))
 
 api.get('/', (req, res) =>{
     res.render('main', {
-
+      
     })
 })
 
-api.get('/incricao', (req, res) => {
+api.get('/inscricao', (req, res) => {
+   
+   const {nome, email, numero} = req.query
+   if(nome && email && numero){
+    const union = inscricao.inscricao(nome,email,numero)
+      res.render('inscricao', {
+        error:false,
+        nome: inscricao.change(nome),
+        email: inscricao.change(email),
+        numero:inscricao.change(numero),
+        union: inscricao.change(union) 
+    })
+} else{
    res.render('inscricao', {
-       
+    error:'Valores invalidos, por favor prencha os campos corretamente'
    })
+}
 })
 
 
